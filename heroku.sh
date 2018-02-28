aabf98069d469cdf794f23c20b1502f457996842
Jan Kondula <jan.kondula@unicorncz.onmicrosoft.com>
2018-02-28 01:04:49 +0100

    tagged!

--- heroku.sh
+++ heroku.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 docker login --username=_ --password=0b713543-b112-4314-9542-99edc61534ab registry.heroku.com
+docker tag api registry.heroku.com/advanced2heroku/web
 docker push registry.heroku.com/advanced2heroku/web
